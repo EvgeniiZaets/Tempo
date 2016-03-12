39e3de78488b52212479541cfac586a811fde99a
Evgenii Zaets <evgeniizaets@gmail.com>
2016-03-12 19:54:53 +0200

    add navigation bar animation

--- nav.js
+++ nav.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-$("td #navbar ul").hide();
-$("td #navbar li span").click(function() {
-    $("td #navbar ul:visible").slideUp("normal");
-    if (($(this).next().is("ul")) && (!$(this).next().is(":visible"))) {
-        $(this).next().slideDown("normal");
-    }
-});
+	$("td #navbar ul").hide();
+	$("td #navbar li span").click(function() {
+		$("td #navbar ul:visible").slideUp("normal");
+		if (($(this).next().is("ul")) && (!$(this).next().is(":visible"))) {
+			$(this).next().slideDown("normal");
+		}
+	});
 });
